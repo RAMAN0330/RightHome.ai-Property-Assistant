0f75f1e77b9a973509f0f2c280e016db22995006
Raman <83901587+RAMAN0330@users.noreply.github.com>
2024-12-14 22:13:47 +0530

    Delete demo.py

--- demo.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBzIZFqiM75Jx3bKNDM_eK4WL6XVlt9XeY")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Explain how AI works")
-print(response.text)
